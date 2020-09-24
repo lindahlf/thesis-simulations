7f2a2fc786510603a1ce37f742e02d3423314ed5
Fred Lindahl <47213636+lindahlf@users.noreply.github.com>
2020-09-24 15:45:11 +0200

    add simulation function

--- thesis-simulations.py
+++ thesis-simulations.py
@@ -4,15 +4,89 @@ import numpy as np
 import random as rnd
 from scipy.stats import norm
 import matplotlib.patches as mpatches
+from tqdm import tqdm
 
-def HC(pvalues):
-    """Compute HC-statistic based on method by Donoho and Jin (2004)"""
+plt.style.use("cyberpunk")
+
+def HC(p_values):
+    """ Compute higher criticism-statistic based on version by Donoho and Jin (2004) """
     p_values = np.sort(p_values) # Make sure p-values are sorted
     n = len(p_values) # Number of data points
-
     ivalues = np.arange(1,int(round(n/2)) + 1)
     p_values = p_values[0:int(round(n/2))] # Cut-off half of the values
-
-    HC_vec = np.sqrt(n)*(ivalues/n - p_values)/np.sqrt(p_values - p_values**2) #Compute for all
-
+    HC_vec = np.sqrt(n)*(ivalues/n - p_values)/np.sqrt(p_values - p_values**2) # Calculate scores for all datapoints
     return np.max(HC_vec)
+
+def phi_test(p_values,s):
+    """ Computes Jager and Wellner's (2007) phi-divergence based test.
+    s = 2 corresponds to standard HC. """
+    v = p_values # use notation of Jager and Wellner
+    n = len(v) # number of datapoints
+    u = np.arange(1,n+1)/n
+    K = v*_phi(s,u/v) + (1-v)*_phi(s,(1-u)/(1-v)) #Calculate score for all datapoints
+    return np.max(K)
+
+def _phi(s,x):
+    """Helper function for phi-divergence test"""
+    if s == 1:
+        return x*np.log(x) - x + 1
+    elif s == 0:
+        return -np.log(x) + x - 1
+    else:
+        return (1-s+s*x-x**s)/(s*(1-s))
+
+# TODO: implement weighted Kolmogorov-Smirnoff statistic
+
+def gaussian_mixture(n,beta,r):
+    """ Returns a sparse Gaussian mixture and a standard Gaussian of size n.
+    Standard corresponds to the null hypothesis,
+    the mixture corresponds to alternative hypothesis. See Donoho and Jin (2004)"""
+    epsilon = n**(-beta)
+    if beta >= 0.5:
+        mu = np.sqrt(2*r*np.log(n))
+    else:
+        mu = n**(-r)
+    null_samples = np.random.normal(0, 1, n)
+    alt_samples1 = null_samples[0:round(n*(1-epsilon))]
+    alt_samples2 = np.random.normal(mu, 1, round(n*epsilon))
+    alt_samples = np.concatenate([alt_samples1,alt_samples2])
+    assert len(null_samples) == len(alt_samples)
+    return null_samples, alt_samples
+
+def calc_pvalue(data):
+    """ Returns one-sided p-value of input array of data """
+    return (1 - norm.cdf(data))
+
+def simulate_scores(n,beta,r,repetitions,func,*args):
+    """ Computes scores for a given detection method for a number of repetitions. """
+    null_scores = [None] * repetitions
+    alt_scores = [None] * repetitions
+    for i in tqdm(range(repetitions)): # Printing progress bar
+        null_samples, alt_samples = gaussian_mixture(n,beta,r)
+        null_p = calc_pvalue(null_samples)
+        alt_p = calc_pvalue(alt_samples)
+        null_scores[i] = func(null_p,*args)
+        alt_scores[i] = func(alt_p,*args)
+    return null_scores, alt_scores
+
+
+################
+# Main program #
+################
+
+# Example use
+
+def main():
+    n = int(1e4)
+    beta = 0.6
+    r = 0.8
+    null_scores, alt_scores = simulate_scores(n,beta,r,100,phi_test,2)
+
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Simulated HC values')
+    axs[0].hist(np.log(null_scores), bins = 100, range = [min(np.log(null_scores)),max(np.log(alt_scores))])
+    axs[1].hist(np.log(alt_scores), bins = 100, range = [min(np.log(null_scores)),max(np.log(alt_scores))])
+    mplcyberpunk.add_glow_effects()
+    plt.show()
+
+main()
