c78038a64bc6959ccefdb007629db969d3982967
Fred Lindahl <47213636+lindahlf@users.noreply.github.com>
2020-09-25 16:38:48 +0200

    add method by Stepanova and Pavlenko

--- thesis-simulations.py
+++ thesis-simulations.py
@@ -20,6 +20,7 @@ def HC(p_values):
 def phi_test(p_values,s):
     """ Computes Jager and Wellner's (2007) phi-divergence based test.
     s = 2 corresponds to standard HC. """
+    p_values = np.sort(p_values)
     v = p_values # use notation of Jager and Wellner
     n = len(v) # number of datapoints
     u = np.arange(1,n+1)/n
@@ -36,6 +37,14 @@ def _phi(s,x):
         return (1-s+s*x-x**s)/(s*(1-s))
 
 # TODO: implement weighted Kolmogorov-Smirnoff statistic
+
+def weight_ks(p_values):
+    """Computes CsCsHM statistic based on results by Stepanova and Pavlenko (2018)"""
+    u = np.sort(p_values) # Assert p-values are sorted, translate to notation of Stepanova and Pavlenko
+    n = len(u) # Number of data points
+    ivalues = np.arange(1,n+1)
+    T = np.sqrt(n)*(ivalues/n - u)/np.sqrt(u*(1-u)*np.log(np.log(1/(u*(1-u)))))
+    return np.max(T)
 
 def gaussian_mixture(n,beta,r):
     """ Returns a sparse Gaussian mixture and a standard Gaussian of size n.
@@ -69,6 +78,7 @@ def simulate_scores(n,beta,r,repetitions,func,*args):
         alt_scores[i] = func(alt_p,*args)
     return null_scores, alt_scores
 
+# TODO: write function to empirically determine critical value for an alpha-level test
 
 ################
 # Main program #
@@ -80,12 +90,14 @@ def main():
     n = int(1e4)
     beta = 0.6
     r = 0.8
-    null_scores, alt_scores = simulate_scores(n,beta,r,100,phi_test,2)
-
+    null_scores, alt_scores = simulate_scores(n,beta,r,100,weight_ks)
+    null_HC, alt_HC = simulate_scores(n,beta,r,100,HC)
     fig, axs = plt.subplots(2)
-    fig.suptitle('Simulated HC values')
-    axs[0].hist(np.log(null_scores), bins = 100, range = [min(np.log(null_scores)),max(np.log(alt_scores))])
-    axs[1].hist(np.log(alt_scores), bins = 100, range = [min(np.log(null_scores)),max(np.log(alt_scores))])
+    fig.suptitle('Simulated scores')
+    axs[0].hist(null_scores, bins = 100, range = [0,100])
+    axs[0].hist(alt_scores, bins = 100, range = [0,100])
+    axs[1].hist(null_HC, bins = 100, range = [0,100])
+    axs[1].hist(alt_HC, bins = 100, range = [0,100])
     mplcyberpunk.add_glow_effects()
     plt.show()
 
